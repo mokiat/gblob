4ddf82cfaf1d342079edf58b7ba4c723a6f6a8c9
Momchil Atanasov <momchil.dev@gmail.com>
2023-02-18 20:36:14 +0200

    Add PackedDecoder concept

---
+++ packed_decoder.go
@@ -0,0 +1,194 @@
+package gblob
+
+import (
+	"fmt"
+	"io"
+	"reflect"
+)
+
+// NewLittleEndianPackedDecoder creates a new PackedDecoder that is configured
+// to read its input in Little Endian order.
+func NewLittleEndianPackedDecoder(in io.Reader) *PackedDecoder {
+	return &PackedDecoder{
+		in: NewLittleEndianReader(in),
+	}
+}
+
+// NewBigEndianPackedDecoder creates a new PackedDecoder that is configured
+// to read its input in Big Endian order.
+func NewBigEndianPackedDecoder(in io.Reader) *PackedDecoder {
+	return &PackedDecoder{
+		in: NewBigEndianReader(in),
+	}
+}
+
+// PackedDecoder decodes arbitrary Go objects from binary form by going through
+// each field in sequence and deserializing it without any padding.
+type PackedDecoder struct {
+	in TypedReader
+}
+
+// Decode decodes the specified target value from the Reader.
+func (d *PackedDecoder) Decode(target interface{}) error {
+	value := reflect.ValueOf(target)
+	if value.Kind() != reflect.Pointer {
+		return fmt.Errorf("target %T is not a pointer", target)
+	}
+	actual := value.Elem()
+	return d.decodeValue(actual)
+}
+
+func (d *PackedDecoder) decodeValue(value reflect.Value) error {
+	switch kind := value.Kind(); kind {
+	case reflect.Pointer:
+		if value.IsNil() {
+			value.Set(reflect.New(value.Type().Elem()))
+		}
+		return d.decodeValue(value.Elem())
+	case reflect.Bool:
+		v, err := d.in.ReadUint8()
+		if err != nil {
+			return err
+		}
+		value.SetBool(v > 0x00)
+		return nil
+	case reflect.Uint8:
+		v, err := d.in.ReadUint8()
+		if err != nil {
+			return err
+		}
+		value.SetUint(uint64(v))
+		return nil
+	case reflect.Int8:
+		v, err := d.in.ReadInt8()
+		if err != nil {
+			return err
+		}
+		value.SetInt(int64(v))
+		return nil
+	case reflect.Uint16:
+		v, err := d.in.ReadUint16()
+		if err != nil {
+			return err
+		}
+		value.SetUint(uint64(v))
+		return nil
+	case reflect.Int16:
+		v, err := d.in.ReadInt16()
+		if err != nil {
+			return err
+		}
+		value.SetInt(int64(v))
+		return nil
+	case reflect.Uint32:
+		v, err := d.in.ReadUint32()
+		if err != nil {
+			return err
+		}
+		value.SetUint(uint64(v))
+		return nil
+	case reflect.Int32:
+		v, err := d.in.ReadInt32()
+		if err != nil {
+			return err
+		}
+		value.SetInt(int64(v))
+		return nil
+	case reflect.Uint64:
+		v, err := d.in.ReadUint64()
+		if err != nil {
+			return err
+		}
+		value.SetUint(uint64(v))
+		return nil
+	case reflect.Int64:
+		v, err := d.in.ReadInt64()
+		if err != nil {
+			return err
+		}
+		value.SetInt(int64(v))
+		return nil
+	case reflect.Float32:
+		v, err := d.in.ReadFloat32()
+		if err != nil {
+			return err
+		}
+		value.SetFloat(float64(v))
+		return nil
+	case reflect.Float64:
+		v, err := d.in.ReadFloat64()
+		if err != nil {
+			return err
+		}
+		value.SetFloat(float64(v))
+		return nil
+	case reflect.Array:
+		count := value.Len()
+		for i := 0; i < count; i++ {
+			if err := d.decodeValue(value.Index(i)); err != nil {
+				return err
+			}
+		}
+		return nil
+	case reflect.Struct:
+		fieldCount := value.NumField()
+		for i := 0; i < fieldCount; i++ {
+			field := value.Field(i)
+			if err := d.decodeValue(field); err != nil {
+				return err
+			}
+		}
+		return nil
+	case reflect.Slice:
+		count, err := d.in.ReadUint64()
+		if err != nil {
+			return err
+		}
+		if value.Type().Elem().Kind() == reflect.Uint8 { // fast track
+			data := make([]uint8, count)
+			if err := d.in.ReadBytes(data); err != nil {
+				return err
+			}
+			value.Set(reflect.ValueOf(data).Convert(value.Type()))
+		} else {
+			value.Set(reflect.MakeSlice(value.Type(), int(count), int(count)))
+			for i := 0; i < int(count); i++ {
+				if err := d.decodeValue(value.Index(i)); err != nil {
+					return err
+				}
+			}
+		}
+		return nil
+	case reflect.Map:
+		count, err := d.in.ReadUint64()
+		if err != nil {
+			return err
+		}
+		value.Set(reflect.MakeMapWithSize(value.Type(), int(count)))
+		for i := 0; i < int(count); i++ {
+			entryKey := reflect.New(value.Type().Key())
+			if err := d.decodeValue(entryKey); err != nil {
+				return err
+			}
+			entryValue := reflect.New(value.Type().Elem())
+			if err := d.decodeValue(entryValue); err != nil {
+				return err
+			}
+			value.SetMapIndex(entryKey.Elem(), entryValue.Elem())
+		}
+		return nil
+	case reflect.String:
+		count, err := d.in.ReadUint64()
+		if err != nil {
+			return err
+		}
+		data := make([]byte, count)
+		if err := d.in.ReadBytes(data); err != nil {
+			return err
+		}
+		value.SetString(string(data))
+		return nil
+	default:
+		return fmt.Errorf("unsupported type: %v", kind)
+	}
+}
--- packed_decoder_test.go
+++ packed_decoder_test.go
@@ -10,10 +10,10 @@ import (
 	"github.com/mokiat/gog"
 )
 
-var _ = Describe("PackedEncoder", func() {
+var _ = Describe("PackedDecoder", func() {
 	var (
 		buffer  *bytes.Buffer
-		encoder *gblob.PackedEncoder
+		decoder *gblob.PackedDecoder
 	)
 
 	seq := func(values ...uint8) []uint8 {
@@ -26,252 +26,274 @@ var _ = Describe("PackedEncoder", func() {
 
 	type CustomString string
 
+	type CustomStruct struct {
+		A uint16
+		B *uint8
+		C uint8
+	}
+
 	BeforeEach(func() {
 		buffer = new(bytes.Buffer)
-		encoder = gblob.NewLittleEndianPackedEncoder(buffer)
+		decoder = gblob.NewLittleEndianPackedDecoder(buffer)
 	})
 
 	DescribeTable("types",
-		func(data any, expected []byte, expectedAlt ...[]byte) {
-			Expect(encoder.Encode(data)).To(Succeed())
-			if len(expectedAlt) == 0 {
-				Expect(buffer.Bytes()).To(Equal(expected))
-			} else {
-				Expect(buffer.Bytes()).To(SatisfyAny(
-					Equal(expected),
-					Equal(expectedAlt[0]),
-				))
-			}
+		func(data []byte, target any, expected any) {
+			buffer.Write(data)
+			Expect(decoder.Decode(target)).To(Succeed())
+			Expect(target).To(Equal(expected))
 		},
 		Entry("bool",
-			bool(true),
 			seq(0x01),
+			gog.PtrOf(false),
+			gog.PtrOf(true),
 		),
 		Entry("*bool",
-			gog.PtrOf(bool(true)),
 			seq(0x01),
+			gog.PtrOf((*bool)(nil)),
+			gog.PtrOf(gog.PtrOf(true)),
 		),
 		Entry("uint8",
-			uint8(0x13),
-			seq(0x13),
+			seq(0x3F),
+			gog.PtrOf(uint8(0x00)),
+			gog.PtrOf(uint8(0x3F)),
 		),
 		Entry("*uint8",
-			gog.PtrOf(uint8(0x13)),
-			seq(0x13),
+			seq(0x3F),
+			gog.PtrOf((*uint8)(nil)),
+			gog.PtrOf(gog.PtrOf(uint8(0x3F))),
 		),
 		Entry("int8",
-			int8(0x13),
-			seq(0x13),
+			seq(0x3F),
+			gog.PtrOf(int8(0x00)),
+			gog.PtrOf(int8(0x3F)),
 		),
 		Entry("*int8",
-			gog.PtrOf(int8(0x13)),
-			seq(0x13),
+			seq(0x3F),
+			gog.PtrOf((*int8)(nil)),
+			gog.PtrOf(gog.PtrOf(int8(0x3F))),
 		),
 		Entry("uint16",
-			uint16(0xF1CA),
 			seq(0xCA, 0xF1),
+			gog.PtrOf(uint16(0x0000)),
+			gog.PtrOf(uint16(0xF1CA)),
 		),
 		Entry("*uint16",
-			gog.PtrOf(uint16(0xF1CA)),
 			seq(0xCA, 0xF1),
+			gog.PtrOf((*uint16)(nil)),
+			gog.PtrOf(gog.PtrOf(uint16(0xF1CA))),
 		),
 		Entry("int16",
-			int16(0x31CA),
 			seq(0xCA, 0x31),
+			gog.PtrOf(int16(0x0000)),
+			gog.PtrOf(int16(0x31CA)),
 		),
 		Entry("*int16",
-			gog.PtrOf(int16(0x31CA)),
 			seq(0xCA, 0x31),
+			gog.PtrOf((*int16)(nil)),
+			gog.PtrOf(gog.PtrOf(int16(0x31CA))),
 		),
 		Entry("uint32",
-			uint32(0xF1CA7632),
 			seq(0x32, 0x76, 0xCA, 0xF1),
+			gog.PtrOf(uint32(0x00000000)),
+			gog.PtrOf(uint32(0xF1CA7632)),
 		),
 		Entry("*uint32",
-			gog.PtrOf(uint32(0xF1CA7632)),
 			seq(0x32, 0x76, 0xCA, 0xF1),
+			gog.PtrOf((*uint32)(nil)),
+			gog.PtrOf(gog.PtrOf(uint32(0xF1CA7632))),
 		),
 		Entry("int32",
-			int32(0x31CA7632),
-			seq(0x32, 0x76, 0xCA, 0x31),
+			seq(0x32, 0x76, 0xCA, 0x51),
+			gog.PtrOf(int32(0x00000000)),
+			gog.PtrOf(int32(0x51CA7632)),
 		),
 		Entry("*int32",
-			gog.PtrOf(int32(0x31CA7632)),
-			seq(0x32, 0x76, 0xCA, 0x31),
+			seq(0x32, 0x76, 0xCA, 0x51),
+			gog.PtrOf((*int32)(nil)),
+			gog.PtrOf(gog.PtrOf(int32(0x51CA7632))),
 		),
 		Entry("uint64",
-			uint64(0xF1CA7632A3C47321),
 			seq(0x21, 0x73, 0xC4, 0xA3, 0x32, 0x76, 0xCA, 0xF1),
+			gog.PtrOf(uint64(0x0000000000000000)),
+			gog.PtrOf(uint64(0xF1CA7632A3C47321)),
 		),
 		Entry("*uint64",
-			gog.PtrOf(uint64(0xF1CA7632A3C47321)),
 			seq(0x21, 0x73, 0xC4, 0xA3, 0x32, 0x76, 0xCA, 0xF1),
+			gog.PtrOf((*uint64)(nil)),
+			gog.PtrOf(gog.PtrOf(uint64(0xF1CA7632A3C47321))),
 		),
 		Entry("int64",
-			int64(0x31CA7632A3C47321),
 			seq(0x21, 0x73, 0xC4, 0xA3, 0x32, 0x76, 0xCA, 0x31),
+			gog.PtrOf(int64(0x0000000000000000)),
+			gog.PtrOf(int64(0x31CA7632A3C47321)),
 		),
 		Entry("*int64",
-			gog.PtrOf(int64(0x31CA7632A3C47321)),
 			seq(0x21, 0x73, 0xC4, 0xA3, 0x32, 0x76, 0xCA, 0x31),
+			gog.PtrOf((*int64)(nil)),
+			gog.PtrOf(gog.PtrOf(int64(0x31CA7632A3C47321))),
 		),
 		Entry("float32",
-			float32(3.7),
 			seq(0xCD, 0xCC, 0x6C, 0x40),
+			gog.PtrOf(float32(0.0)),
+			gog.PtrOf(float32(3.7)),
 		),
 		Entry("*float32",
-			gog.PtrOf(float32(3.7)),
 			seq(0xCD, 0xCC, 0x6C, 0x40),
+			gog.PtrOf((*float32)(nil)),
+			gog.PtrOf(gog.PtrOf(float32(3.7))),
 		),
 		Entry("float64",
-			float64(3.7),
 			seq(0x9A, 0x99, 0x99, 0x99, 0x99, 0x99, 0x0D, 0x40),
+			gog.PtrOf(float64(0.0)),
+			gog.PtrOf(float64(3.7)),
 		),
 		Entry("*float64",
-			gog.PtrOf(float64(3.7)),
 			seq(0x9A, 0x99, 0x99, 0x99, 0x99, 0x99, 0x0D, 0x40),
+			gog.PtrOf((*float64)(nil)),
+			gog.PtrOf(gog.PtrOf(float64(3.7))),
 		),
 		Entry("array",
-			[3]uint16{0x31FA, 0x45AC, 0x5F21},
 			seq(0xFA, 0x31, 0xAC, 0x45, 0x21, 0x5F),
+			gog.PtrOf([3]uint16{}),
+			gog.PtrOf([3]uint16{0x31FA, 0x45AC, 0x5F21}),
 		),
 		Entry("*array",
-			gog.PtrOf([3]uint16{0x31FA, 0x45AC, 0x5F21}),
 			seq(0xFA, 0x31, 0xAC, 0x45, 0x21, 0x5F),
+			gog.PtrOf((*[3]uint16)(nil)),
+			gog.PtrOf(gog.PtrOf([3]uint16{0x31FA, 0x45AC, 0x5F21})),
 		),
 		Entry("CustomArray",
-			CustomArray([3]uint16{0x31FA, 0x45AC, 0x5F21}),
 			seq(0xFA, 0x31, 0xAC, 0x45, 0x21, 0x5F),
+			gog.PtrOf(CustomArray{}),
+			gog.PtrOf(CustomArray{0x31FA, 0x45AC, 0x5F21}),
 		),
 		Entry("*CustomArray",
-			gog.PtrOf(CustomArray([3]uint16{0x31FA, 0x45AC, 0x5F21})),
 			seq(0xFA, 0x31, 0xAC, 0x45, 0x21, 0x5F),
+			gog.PtrOf((*CustomArray)(nil)),
+			gog.PtrOf(gog.PtrOf(CustomArray{0x31FA, 0x45AC, 0x5F21})),
 		),
-		Entry("bytes", // fast track
-			[]uint8{0x31, 0xFA, 0x45, 0xAC, 0x5F, 0x21},
+		Entry("bytes",
 			seq(
 				0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // length
 				0x31, 0xFA, 0x45, 0xAC, 0x5F, 0x21, // items
 			),
-		),
-		Entry("*bytes", // fast track
+			gog.PtrOf([]uint8(nil)),
 			gog.PtrOf([]uint8{0x31, 0xFA, 0x45, 0xAC, 0x5F, 0x21}),
+		),
+		Entry("*bytes",
 			seq(
 				0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // length
 				0x31, 0xFA, 0x45, 0xAC, 0x5F, 0x21, // items
 			),
+			gog.PtrOf((*[]uint8)(nil)),
+			gog.PtrOf(gog.PtrOf([]uint8{0x31, 0xFA, 0x45, 0xAC, 0x5F, 0x21})),
 		),
 		Entry("slice",
-			[]uint16{0x31FA, 0x45AC, 0x5F21},
 			seq(
 				0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // length
 				0xFA, 0x31, 0xAC, 0x45, 0x21, 0x5F, // items
 			),
+			gog.PtrOf([]uint16(nil)),
+			gog.PtrOf([]uint16{0x31FA, 0x45AC, 0x5F21}),
 		),
 		Entry("*slice",
-			gog.PtrOf([]uint16{0x31FA, 0x45AC, 0x5F21}),
 			seq(
 				0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // length
 				0xFA, 0x31, 0xAC, 0x45, 0x21, 0x5F, // items
 			),
+			gog.PtrOf((*[]uint16)(nil)),
+			gog.PtrOf(gog.PtrOf([]uint16{0x31FA, 0x45AC, 0x5F21})),
 		),
 		Entry("CustomSlice",
-			CustomSlice([]uint16{0x31FA, 0x45AC, 0x5F21}),
 			seq(
 				0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // length
 				0xFA, 0x31, 0xAC, 0x45, 0x21, 0x5F, // items
 			),
+			gog.PtrOf(CustomSlice(nil)),
+			gog.PtrOf(CustomSlice{0x31FA, 0x45AC, 0x5F21}),
 		),
 		Entry("*CustomSlice",
-			gog.PtrOf(CustomSlice([]uint16{0x31FA, 0x45AC, 0x5F21})),
 			seq(
 				0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // length
 				0xFA, 0x31, 0xAC, 0x45, 0x21, 0x5F, // items
 			),
+			gog.PtrOf((*CustomSlice)(nil)),
+			gog.PtrOf(gog.PtrOf(CustomSlice{0x31FA, 0x45AC, 0x5F21})),
 		),
 		Entry("map",
-			map[uint8]uint16{
-				0xAC: 0x37FA,
-				0x05: 0x51A2,
-			},
 			seq(
 				0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // length
 				0xAC, 0xFA, 0x37, // first entry
 				0x05, 0xA2, 0x51, // second entry
 			),
-			seq(
-				0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // length
-				0x05, 0xA2, 0x51, // second entry
-				0xAC, 0xFA, 0x37, // first entry
-			),
-		),
-		Entry("*map",
+			gog.PtrOf(map[uint8]uint16(nil)),
 			gog.PtrOf(map[uint8]uint16{
 				0xAC: 0x37FA,
 				0x05: 0x51A2,
 			}),
+		),
+		Entry("*map",
 			seq(
 				0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // length
 				0xAC, 0xFA, 0x37, // first entry
 				0x05, 0xA2, 0x51, // second entry
 			),
-			seq(
-				0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // length
-				0x05, 0xA2, 0x51, // second entry
-				0xAC, 0xFA, 0x37, // first entry
-			),
+			gog.PtrOf((*map[uint8]uint16)(nil)),
+			gog.PtrOf(gog.PtrOf(map[uint8]uint16{
+				0xAC: 0x37FA,
+				0x05: 0x51A2,
+			})),
 		),
 		Entry("string",
-			"hello",
 			seq(
 				0x05, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // length
 				0x68, 0x65, 0x6C, 0x6C, 0x6F, // items
 			),
+			gog.PtrOf(""),
+			gog.PtrOf("hello"),
 		),
 		Entry("*string",
-			gog.PtrOf("hello"),
 			seq(
 				0x05, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // length
 				0x68, 0x65, 0x6C, 0x6C, 0x6F, // items
 			),
+			gog.PtrOf((*string)(nil)),
+			gog.PtrOf(gog.PtrOf("hello")),
 		),
 		Entry("CustomString",
-			CustomString("hello"),
 			seq(
 				0x05, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // length
 				0x68, 0x65, 0x6C, 0x6C, 0x6F, // items
 			),
+			gog.PtrOf(CustomString("")),
+			gog.PtrOf(CustomString("hello")),
 		),
 		Entry("*CustomString",
-			gog.PtrOf(CustomString("hello")),
 			seq(
 				0x05, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // length
 				0x68, 0x65, 0x6C, 0x6C, 0x6F, // items
 			),
+			gog.PtrOf((*CustomString)(nil)),
+			gog.PtrOf(gog.PtrOf(CustomString("hello"))),
 		),
 		Entry("struct",
-			struct {
-				A uint16
-				B uint8
-				C uint8
-			}{
+			seq(0x66, 0x55, 0xFF, 0x01),
+			&CustomStruct{},
+			&CustomStruct{
 				A: 0x5566,
-				B: 0xFF,
+				B: gog.PtrOf(uint8(0xFF)),
 				C: 0x01,
 			},
-			seq(0x66, 0x55, 0xFF, 0x01),
 		),
 		Entry("*struct",
-			gog.PtrOf(struct {
-				A uint16
-				B uint8
-				C uint8
-			}{
+			seq(0x66, 0x55, 0xFF, 0x01),
+			gog.PtrOf((*CustomStruct)(nil)),
+			gog.PtrOf(&CustomStruct{
 				A: 0x5566,
-				B: 0xFF,
+				B: gog.PtrOf(uint8(0xFF)),
 				C: 0x01,
 			}),
-			seq(0x66, 0x55, 0xFF, 0x01),
 		),
 	)
 })
